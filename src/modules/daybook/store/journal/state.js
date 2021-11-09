ed4bed0c420f9a235d5622039ddc518bc7be4c2e
seraleman <seraleman1204@gmail.com>
2021-11-09 11:53:27 -0500

    Primer commit

---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,23 @@
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: 'Mollit anim deserunt sunt proident ex mollit id laborum voluptate et non non id excepteur. Sit incididunt exercitation commodo dolor enim sint. Esse officia aliquip amet est nulla magna est veniam culpa sit nisi ut reprehenderit magna. Et laborum sit aliqua voluptate nisi sunt exercitation incididunt labore occaecat minim. Officia excepteur tempor amet irure dolor eu est id incididunt esse.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 100,
+            date: new Date().toDateString(),
+            text: 'Aliquip consequat esse ut cillum do et minim minim cupidatat consequat laboris voluptate dolore magna. Eu voluptate qui veniam laboris do et magna et reprehenderit eiusmod minim esse ut. Cillum irure ullamco excepteur magna dolore esse qui ullamco. Exercitation ut ut duis commodo ex nisi labore. Pariatur in ad aute ipsum minim sint aliqua exercitation veniam et. Do minim labore occaecat do occaecat.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 200,
+            date: new Date().toDateString(),
+            text: 'Ipsum nulla ipsum reprehenderit consectetur ex et officia est irure. Enim laborum et consectetur labore quis officia ea Lorem aute eu adipisicing. Qui voluptate sit laboris occaecat anim minim cupidatat ut amet. Eu dolor culpa consequat id aliquip. Nulla ex commodo tempor reprehenderit sit fugiat voluptate sint id. Est sunt commodo velit minim irure ut irure sunt aliqua veniam irure. Eiusmod qui officia nisi voluptate adipisicing culpa ipsum est consequat veniam.',
+            picture: null,
+        },
+    ]
+})
